ca51e4e01917f408232ca701e6277a9b0422091b
Yessimkhan <200103408@stu.sdu.edu.kz>
2024-07-07 15:19:21 +0500

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Yessimkhan Zhumash on 07.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
